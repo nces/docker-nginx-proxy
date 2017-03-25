dfb3e4bcc2e75c97fe8a7d3da0aa787d2c519efc
Charlie McClung <charlie@cmr1.com>
2017-03-24 22:42:19 -0600

    Update deploy with branch targets

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,19 @@
 set -e
 
 REPO=cmr1/nginx-proxy
+BRANCH_TARGETS=("php-fpm" "open-cors")
 
-function push() {
+should_deploy_branch() {
+  for i in "${BRANCH_TARGETS[@]}"; do
+    if [[ "$1" == "$i" ]]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+push() {
   # Get the tag from argument to this function
   TAG="${1:-latest}"
 
@@ -24,4 +35,6 @@ if [ ! -z "$TRAVIS_TAG" ]; then
   push $TRAVIS_TAG
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
   push latest
+elif should_deploy_branch $TRAVIS_BRANCH; then
+  push $TRAVIS_BRANCH
 fi
