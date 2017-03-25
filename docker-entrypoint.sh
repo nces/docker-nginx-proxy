ab12f3443ad2a9a5dcb51128bc2e828c921babf0
Charlie McClung <charlie@cmr1.com>
2017-03-24 23:09:30 -0600

    Supporting generic php-fpm nginx configuration(s)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,25 +4,14 @@
 set -e
 
 LOCATIONS=()
-UPSTREAMS=()
 
 LOCATIONS_FILE=$NGINX_INSTALL_PATH/conf.d/locations.conf
-UPSTREAMS_FILE=$NGINX_INSTALL_PATH/conf.d/upstreams.conf
 
 # Update placeholder vars to be taken from ENV vars
 
 PLACEHOLDER_SERVER_NAME="${SERVER_NAME:-_}"
 PLACEHOLDER_SERVER_TYPE="${SERVER_TYPE:-http}"
-
-upstream_exists () {
-  for i in "${UPSTREAMS[@]}"; do
-    if [[ "$1" == "$i" ]]; then
-      return 0
-    fi
-  done
-
-  return 1
-}
+PLACEHOLDER_SERVER_ROOT="${SERVER_ROOT:-/var/www/html}"
 
 location_exists () {
   for i in "${LOCATIONS[@]}"; do
@@ -34,24 +23,15 @@ location_exists () {
   return 1
 }
 
-function create_upstream() {
-cat <<EOF
-upstream $1 {
-  server $2:$3;
-}
-EOF
-}
-
 function create_location() {
 cat <<EOF
-location ~ ^$1 {
-  rewrite $1(.*) $3\$1 break;
-  proxy_set_header Host \$http_host;
-  proxy_set_header X-Real-IP \$remote_addr;
-  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_set_header X-Forwarded-Proto \$scheme;
-  proxy_redirect off;
-  proxy_pass http://$2;
+location ~ ^$1.+\.php\$ {
+    fastcgi_index index.php;
+    fastcgi_pass $2:$3;
+    fastcgi_buffers 16 16k;
+    fastcgi_buffer_size 32k;
+    fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
+    include fastcgi_params;
 }
 EOF
 }
@@ -59,7 +39,6 @@ EOF
 if [[ "$SERVER_BACKENDS" == "" ]]; then
   echo "No backends defined!"
 else
-  echo "" > $UPSTREAMS_FILE
   echo "" > $LOCATIONS_FILE
 
   IFS=',' read -ra PROVIDED_BACKENDS <<< "$SERVER_BACKENDS"
@@ -72,24 +51,15 @@ else
       path="${SETTINGS[0]}"
       cont="${SETTINGS[1]}"
       port="${SETTINGS[2]}"
-      dest="${SETTINGS[3]:-$path}"
-      upstream="$cont-$port"
 
-      echo "Configuring NGINX route: '$path' => '$cont:$port$dest' (using upstream: '$upstream')"
-      
-      if upstream_exists $upstream; then
-        echo "WARNING! Duplicate upstream: '$upstream' for backend: '$backend' :: Using existing upstream definition..."
-      else
-        UPSTREAMS+=("$upstream")      
-        echo "$(create_upstream $upstream $cont $port)" >> $UPSTREAMS_FILE
-      fi
+      echo "Configuring NGINX PHP-FPM route: '$path' => '$cont:$port'"
 
       if location_exists $path; then
         echo "ERROR! Location conflict: '$path' is already registered!"
         exit 1
       else
         LOCATIONS+=("$path")
-        echo "$(create_location $path $upstream $dest)" >> $LOCATIONS_FILE
+        echo "$(create_location $path $cont $port)" >> $LOCATIONS_FILE
       fi
     fi
   done
@@ -99,6 +69,7 @@ sed -i "s/PLACEHOLDER_SERVER_TYPE/${PLACEHOLDER_SERVER_TYPE}/g" "${NGINX_INSTALL
 
 for conf in $NGINX_INSTALL_PATH/conf.d/*.conf; do
   sed -i "s/PLACEHOLDER_SERVER_NAME/${PLACEHOLDER_SERVER_NAME}/g" "${conf}"
+  sed -i "s;PLACEHOLDER_SERVER_ROOT;${PLACEHOLDER_SERVER_ROOT};g" "${conf}"
 done 
 
 # Execute the CMD from the Dockerfile and pass in all of its arguments.
